986636afcaa53798d8914d343afedadc35295e8a
gururajchadaga <guruuraj.chadaga@gmail.com>
2021-08-16 18:25:45 +0530

    Fixed CartProvider's cartContext

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,12 +2,12 @@ import CartContext from './cart-context';
 const CartProvider = (props) => {
   const addItemToCartHandler = (item) => {};
   const removeItemFromCartHandler = (id) => {};
-  const cartContext = createContext({
+  const cartContext = {
     items: [],
     totalAmount: 0,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
-  });
+  };
 
   return (
     <CartContext.Provider value={cartContext}>
